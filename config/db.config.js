b2e092430a45e2e47bbf497cc795b758c94466f5
hasan dalkilic <hasan.dalkilic@n2mobil.com.tr>
2022-04-23 22:06:59 +0300

    add prettier conf

--- config/db.config.js
+++ config/db.config.js
@@ -1,17 +1,17 @@
-const { Sequelize } = require("sequelize");
+const { Sequelize } = require('sequelize')
 
-const sequelize = new Sequelize("node_db", "root", "bildes21", {
-  host: "localhost",
-  dialect: "mysql",
-});
+const sequelize = new Sequelize('node_db', 'root', 'bildes21', {
+    host: 'localhost',
+    dialect: 'mysql',
+})
 
 const connectDb = async () => {
-  try {
-    await sequelize.authenticate();
-    console.log("Database Connection has been established successfully.");
-  } catch (error) {
-    console.error("Unable to connect to the database:", error);
-  }
-};
+    try {
+        await sequelize.authenticate()
+        console.log('Database Connection has been established successfully.')
+    } catch (error) {
+        console.error('Unable to connect to the database:', error)
+    }
+}
 
 module.exports = connectDb
